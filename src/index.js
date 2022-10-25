58b2c9b6136ff548dfbcc2230ade01d16272ce7c
EdenSpacek <space.taitt99@gmail.com>
2022-10-25 10:57:14 +0700

    Product

--- src/index.js
+++ src/index.js
@@ -4,11 +4,17 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
 import "remixicon/fonts/remixicon.css";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 import "./index.css";
+
+import { BrowserRouter as Router } from 'react-router-dom'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
